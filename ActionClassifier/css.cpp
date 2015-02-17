29c5502c0c5373869801c0fda176903bd66d85bb
tjacek <tjacek@student.agh.edu.pl>
2015-02-17 18:42:49 +0100

    preprocessing image

--- ActionClassifier/css.cpp
+++ ActionClassifier/css.cpp
@@ -3,7 +3,8 @@
  vector<Point>* extractCSSFeatures(Mat * image){
    vector<Point>* points=new vector<Point>();
    detectEdge(image);
-   getCurve(image);
+   Curve* curve=getCurve(image);
+  // curve->smooth(2.0);
    return points;
  }
 
@@ -11,17 +12,24 @@ static const double minThreshold = 30.0;
 static const double maxThreshold = 50.0;
 
 void detectEdge(Mat * mat){
-  cv::Canny(*mat, *mat, minThreshold, maxThreshold);
+  cv:blur(*mat,*mat,cv::Size(20,20));
+  cv::threshold(*mat,*mat,128,255,0);
+  cv::Canny(*mat, *mat, minThreshold, maxThreshold); 
 }
 
 Curve * getCurve(Mat * image){
   vector<vector<cv::Point> > contours;
   vector<cv::Vec4i> hierarchy;
 
-  cv::findContours(*image, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
-  vector<cv::Point> countur=contours[0];
-  Curve * curve = new Curve(countur);
-  return curve;
+  	 cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );
+	 cv::imshow( "Display window", *image );                  
+     cv::waitKey(0);
+
+   cv::findContours(*image, contours, hierarchy,CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+   draw( *image, &contours, &hierarchy);
+  //vector<cv::Point> countur=contours.at(0);
+  //Curve * curve = new Curve(countur);
+  return NULL;//curve;
 }
 
 Curve::Curve(vector<cv::Point> countur){
@@ -33,10 +41,38 @@ Curve::Curve(vector<cv::Point> countur){
   {
      Point point=*it;
 	 x_vec.push_back(point.x);
-	 x_vec.push_back(point.y);
+	 y_vec.push_back(point.y);
   }
   x=new Mat(x_vec);
   y=new Mat(y_vec);
 }
 
+void Curve::smooth(double sigma){
+  smooth_x=new Mat();
+  smooth_y=new Mat();
+  cout << *x;
+  gauss_conv(*x,*smooth_x, sigma);
+  gauss_conv(*y,*smooth_y, sigma);
+}
 
+void gauss_conv(Mat in,Mat out,double sigma){
+  int width=in.rows;
+  Mat G;
+  cv::transpose(cv::getGaussianKernel(width, sigma, CV_64FC1), G);
+ // cout << G;
+//  filter2D(in, out, out.depth(), G);
+}
+
+void draw(Mat image,vector<vector<cv::Point> > *contours,vector<cv::Vec4i> *hierarchy){
+  Mat drawing = Mat::zeros( image.size(), CV_8UC3 );
+  //for( int i = 0; i< contours.size(); i++ )
+  //   {
+       cv::Scalar color = cv::Scalar( rand() % 255, rand() % 255, rand() % 255 );
+       drawContours( drawing, *contours, -1, color, 2, 8, *hierarchy, 0, Point() );
+  //   }
+
+  cv::namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+  imshow( "Contours", drawing );
+  cv::waitKey(0);
+
+}
--- ActionClassifier/css.h
+++ ActionClassifier/css.h
@@ -5,9 +5,14 @@ class Curve{
  public:
    Mat * x;
    Mat * y;
+   Mat * smooth_x;
+   Mat * smooth_y;
    Curve(vector<cv::Point> countur);
+   void smooth(double sigma);
 };
 
 extern vector<Point>* extractCSSFeatures(Mat * image);
 extern void detectEdge(Mat * mat);
 extern Curve * getCurve(Mat * image);
+extern void gauss_conv(Mat in,Mat out,double sigma);
+extern void draw(Mat image,vector<vector<cv::Point> >* contours,vector<cv::Vec4i>* hierarchy);
