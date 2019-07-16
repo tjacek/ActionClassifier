8770d7a6f79b4d1aad83f3f44f421e5f015c0ea2
tjacek <tjacek@agh.edu.pl>
2019-07-16 20:02:49 +0200

    start rewirte

--- ActionClassifier/transform.h
+++
@@ -1,8 +0,0 @@
-#include "ActionClassifier.h"
-#include "utils.h"
-
-extern Mat cleanEdge(Mat * m);
-extern void removeEdge(Mat * m);
-extern double mediana(vector<double> values);
-extern double getPointMediana(int i,int j,Mat* image);
-extern Mat medianaFilter(Mat * image);
