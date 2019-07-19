fbbbee2f5b1851b5acc745d386a5d4586d2e0b63
tjacek <tjacek@student.agh.edu.pl>
2019-07-19 17:53:09 +0200

    learn convnet

--- convnet.py
+++ convnet.py
@@ -4,12 +4,27 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import numpy as np
 from scipy.interpolate import CubicSpline
+from sklearn.metrics import classification_report
 import data
+
+def simple_exp(in_path,n_epochs=100):
+    train_X,train_y,test_X,test_y,params=prepare_data(in_path)
+    model=make_conv(params)
+    model.summary()
+    #raise Exception(train_X.shape)
+    model.fit(train_X,train_y,epochs=n_epochs,batch_size=32)
+    raw_pred=model.predict(test_X,batch_size=32)
+    pred_y,test_y=np.argmax(raw_pred,axis=1),np.argmax(test_y,axis=1)
+    print(classification_report(test_y, pred_y,digits=4))
 
 def prepare_data(in_path,n=128):
     (train_X,train_y),(test_X,test_y)=data.get_dataset(in_path)
+    params={'ts_len':n,
+            'n_feats':train_X[0].shape[1],
+            'n_cats':np.unique(train_y).shape[0]}    
     train_X,train_y=format_data(train_X,train_y) 
-    test_X,test_y=format_data(train_X,train_y) 
+    test_X,test_y=format_data(test_X,test_y) 
+    return train_X,train_y,test_X,test_y,params
 
 def format_data(X,y,new_size=128):
     new_X,new_y=[],keras.utils.to_categorical(y)
@@ -25,20 +40,28 @@ def format_data(X,y,new_size=128):
             x_i=cs_ij(new_range)
             new_X[-1].append(x_i)
     new_X= np.swapaxes(np.array(new_X),1,2)
+    new_X= np.expand_dims(new_X,-1)
     return new_X,new_y
 
 def make_conv(params):
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3),
+    model.add(Conv2D(8, kernel_size=(8, 1),
                  activation='relu',
-                 input_shape=(28,28,1)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+                 input_shape=(params['ts_len'],params['n_feats'],1)))
+    model.add(MaxPooling2D(pool_size=(4, 1)))
+    model.add(Conv2D(8, kernel_size=(8, 1),
+                 activation='relu',
+                 input_shape=(params['ts_len'],params['n_feats'],1)))
+    model.add(MaxPooling2D(pool_size=(4, 1)))
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(100, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(units=params['n_cats'], activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+              #optimizer=keras.optimizers.Adadelta(),
+              optimizer=keras.optimizers.SGD(lr=0.01,  momentum=0.9, nesterov=True))
+    #          metrics=['accuracy'])
     return model
 
-prepare_data("mra")
+simple_exp("mra")
