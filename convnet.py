833596e6a53bb2b5cd7df9fd56b9f532b83902d2
tjacek <tjacek@student.agh.edu.pl>
2019-07-19 22:38:48 +0200

    extract convolutional features

--- convnet.py
+++ convnet.py
@@ -10,6 +10,22 @@ def simple_exp(in_path,n_epochs=100):
     utils.simple_exp(in_path,n_epochs=n_epochs,
                         prepare=prepare_data,make_model=make_conv)
 
+def extract_features(in_path,out_path,n_epochs=100):
+    utils.extract_features(in_path,out_path,n_epochs=n_epochs,
+                        prepare=prepare_data,make_model=make_conv,
+                        extraction=conv_extraction)
+
+def conv_extraction(data_dict,extractor_model):
+    new_size=extractor_model.layers[0].input_shape[1]
+    new_range=np.arange(new_size) 
+    def seq_helper(x_i):
+        x_i=spline_intepolation(x_i,new_size,new_range)
+        x_i=np.expand_dims(x_i,axis=0)
+        x_i=np.expand_dims(x_i,axis=-1)
+        return extractor_model.predict(x_i)
+    return {name_i:seq_helper(data_i)
+                for name_i,data_i in data_dict.items()}
+
 def prepare_data(in_path,n=128):
     (train_X,train_y),(test_X,test_y)=data.get_dataset(in_path)
     params={'ts_len':n,
@@ -22,19 +38,22 @@ def prepare_data(in_path,n=128):
 def format_data(X,y,new_size=128):
     new_X,new_y=[],keras.utils.to_categorical(y)
     new_range=np.arange(new_size) 
-    for i,x_i in enumerate(X):
-    	old_size=x_i.shape[0]
-        old_range=np.arange(old_size).astype(float)
-        step=float(new_size)/float(old_size)
-        old_range*=step
-        new_X.append([])
-        for feat_j in x_i.T:
-            cs_ij=CubicSpline(old_range,feat_j)
-            x_i=cs_ij(new_range)
-            new_X[-1].append(x_i)
-    new_X= np.swapaxes(np.array(new_X),1,2)
+    new_X=[spline_intepolation(x_i,new_size,new_range) for x_i in X]    	
+    #new_X= np.swapaxes(np.array(new_X),1,2)
     new_X= np.expand_dims(new_X,-1)
     return new_X,new_y
+
+def spline_intepolation(x_i,new_size,new_range):
+    old_size=x_i.shape[0]
+    old_range=np.arange(old_size).astype(float)
+    step=float(new_size)/float(old_size)
+    old_range*=step
+    new_x_i=[]
+    for feat_j in x_i.T:
+        cs_ij=CubicSpline(old_range,feat_j)
+        x_i=cs_ij(new_range)
+        new_x_i.append(x_i)
+    return np.array(new_x_i).T
 
 def make_conv(params):
     model = Sequential()
@@ -48,8 +67,8 @@ def make_conv(params):
     model.add(MaxPooling2D(pool_size=(4, 1)))
     model.add(Dropout(0.25))
     model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
-    #model.add(Dropout(0.5))
+    model.add(Dense(100, activation='relu',name="hidden"))
+    model.add(Dropout(0.5))
     model.add(Dense(units=params['n_cats'], activation='softmax'))
     model.compile(loss=keras.losses.categorical_crossentropy,
               #optimizer=keras.optimizers.Adadelta(),
@@ -58,3 +77,4 @@ def make_conv(params):
     return model
 
 simple_exp("mra")
+#extract_features("mra","conv.txt",n_epochs=100)
--- utils.py
+++ utils.py
@@ -4,14 +4,14 @@ from sklearn.metrics import classification_report
 import data
 
 def simple_exp(in_path,prepare,make_model,n_epochs=10):
-    model=train_model(in_path,prepare,make_model,n_epochs=n_epochs)
+    model,(test_X,test_y)=train_model(in_path,prepare,make_model,n_epochs=n_epochs)
     raw_pred=model.predict(test_X,batch_size=32)
     pred_y,test_y=np.argmax(raw_pred,axis=1),np.argmax(test_y,axis=1)
     print(classification_report(test_y, pred_y,digits=4))
 
 def extract_features(in_path,out_path,prepare,
 	                        make_model,extraction,n_epochs=100):
-    model=train_model(in_path,prepare,make_model,n_epochs=n_epochs)
+    model,(test_X,test_y)=train_model(in_path,prepare,make_model,n_epochs=n_epochs)
     extractor_model=make_extractor(model)
     data_dict=data.get_dataset(in_path,splited=False)
     feat_dict=extraction(data_dict,extractor_model)
@@ -22,7 +22,7 @@ def train_model(in_path,prepare,make_model,n_epochs=100):
     model=make_model(params)
     model.summary()
     model.fit(train_X,train_y,epochs=n_epochs,batch_size=32)
-    return model
+    return model,(test_X,test_y)
 
 def make_extractor(model):
     return Model(inputs=model.input,
