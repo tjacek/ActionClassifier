f268c7d3a2acfd8e3e56e2b5ba3c046ffddefb13
tjacek <tjacek@student.agh.edu.pl>
2019-07-28 13:53:45 +0200

    plot loss and accuracy

--- smooth.py
+++ smooth.py
@@ -3,6 +3,7 @@ import keras.utils
 from scipy.signal import gaussian
 from scipy.ndimage import filters
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 import data,convnet
 
 class GaussSmooth(object):
@@ -13,18 +14,28 @@ class GaussSmooth(object):
     def __call__(self,ts_i):
         return filters.convolve1d(ts_i,self.filtr)
 
-def smooth_curve(in_path):
+def smooth_curve(in_path,n_epochs=100):
     data_dict=data.get_dataset(in_path,splited=False)
-    smooth_data=smooth_dataset(data_dict,GaussSmooth())
-    def conv_helper(smooth_data):
-        train_X,train_y,test_X,test_y,params=convnet.prepare_data(smooth_data)
-        model=convnet.make_conv(params)
-        history=model.fit(train_X,train_y,epochs=100,batch_size=32)
-        loss_i=history.history['loss'][-1]
-        pred_y=model.predict_classes(test_X)
-        true_y=np.argmax(test_y,axis=1)
-        return loss_i, accuracy_score(true_y,pred_y)
-    print(conv_helper(smooth_data))
+    loss,acc=[],[]
+    sigma=np.arange(1,10)
+    for i in sigma:    
+        smooth_i=smooth_dataset(data_dict,GaussSmooth(i))
+        loss_i,acc_i=conv_helper(smooth_i,n_epochs)
+        loss.append(loss_i)
+        acc.append(acc_i)
+    plt.plot(sigma,loss)
+    plt.show()
+    plt.plot(sigma,acc)
+    plt.show()
+
+def conv_helper(smooth_data,n_epochs=10):
+    train_X,train_y,test_X,test_y,params=convnet.prepare_data(smooth_data)
+    model=convnet.make_conv(params)
+    history=model.fit(train_X,train_y,epochs=n_epochs,batch_size=32)
+    loss_i=history.history['loss'][-1]
+    pred_y=model.predict_classes(test_X)
+    true_y=np.argmax(test_y,axis=1)
+    return loss_i, accuracy_score(true_y,pred_y)
 
 def smooth_dataset(data_dict,smooth): 
     return { name_i:smooth_feature(sample_i,smooth)
