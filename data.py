a18a15b04fcad31d72e85eafaee8aa56a094454f
tjacek <tjacek@agh.edu.pl>
2020-11-22 21:07:55 +0100

    split dataset

--- data.py
+++ data.py
@@ -54,23 +54,6 @@ def read_seq(path_i,postfix=False):
     data_i=np.loadtxt(path_i,dtype=float,delimiter=",")
     return (name_i,data_i)
 
-def split(data_dict):
-    train,test={},{}
-    for name_i in data_dict.keys():
-        person_i=int(name_i.split('_')[1])
-        if(person_i%2==1):
-            train[name_i]=data_dict[name_i]
-        else:
-            test[name_i]=data_dict[name_i]
-    return train,test
-
-def to_array(data_dict):
-    names=sorted(data_dict.keys(),key=natural_keys) 
-    X=[data_dict[name_i] for name_i in names]
-    y=[ int(name_i.split('_')[0])-1 for name_i in names]
-    return X,y
-
-
 def show_seq_len(data_dict):
     for name_i,data_i in data_dict.items():
         print((name_i+" %d %d") % data_i.shape )
--- seqs.py
+++ seqs.py
@@ -5,6 +5,27 @@ class Seqs(dict):
 	def __init__(self):
 		super(Seqs, self).__init__()
 
+	def split(self,selector=None):
+		if(not selector):
+			selector=person_selector
+		train,test=Seqs(),Seqs()
+		for name_i in self.keys():
+			if(selector(name_i)):
+				train[name_i]=self[name_i]
+			else:
+				test[name_i]=self[name_i]
+		return train,test
+
+	def to_dataset(self):
+		names=sorted(self.keys(),key=files.natural_keys) 
+		X=[self[name_i] for name_i in names]
+		y=[ int(name_i.split('_')[0])-1 for name_i in names]
+		return X,y
+
+def person_selector(name_i):
+	person_i=int(name_i.split('_')[1])
+	return person_i%2==1
+
 def read_seqs(in_path):
 	paths=files.top_files(in_path)
 	seq_dict=Seqs()
@@ -15,4 +36,5 @@ def read_seqs(in_path):
 		seq_dict[name_i]=data_i
 	return seq_dict
 
-read_seqs("Data/MSR/common/seqs")	
+s=read_seqs("Data/MSR/common/seqs")	
+print(s.to_dataset()[1])
