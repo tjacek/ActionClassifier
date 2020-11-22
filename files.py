501a3276d61dc12910c9944b1ad17827348c9635
tjacek <tjacek@agh.edu.pl>
2020-11-22 21:39:34 +0100

    recursive transform

--- files.py
+++ files.py
@@ -19,12 +19,33 @@ def natural_keys(text):
 def atoi(text):
     return int(text) if text.isdigit() else text
 
+def make_path(path):
+    paths=path.split("/")
+    for i in range(len(paths)):
+        path_i="/".join(paths[:i+1])
+        print(path_i)
+        make_dir(path_i)
+
 def make_dir(path):
     if(not os.path.isdir(path)):
         os.mkdir(path)
 
+def replace_path(in_path,out_path):
+    paths=in_path.split('/')[1:]
+    paths=[out_path]+paths
+    return "/".join(paths)
+
 def clean(name_i):
     raw=name_i.split('_')
-#    ending= raw[-1] if(postfix and len(raw)>3) else ''
     name_i=re.sub(r'\D0','',name_i.strip())
     return "_".join(re.findall(r'\d+',name_i))
+
+def recursive_transform(in_path,out_path,name):
+    for (root,dirs,files) in os.walk(in_path, topdown=True): 
+        if(root.split('/')[-1]==name):
+            out_i=replace_path(root,out_path)            
+            make_path(out_i)
+            print("--------------")    
+            print(root)
+            print(dirs)
+            print(out_i)
