b78ed5a0cef6aa1033b94cf1487bc07f908b28cd
tjacek <tjacek@agh.edu.pl>
2020-12-03 21:23:29 +0100

    save imgs

---
+++ agum.py
@@ -0,0 +1,53 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import keras.utils
+from keras import regularizers
+from keras.models import load_model
+import numpy as np
+import imgs
+
+def train_model(in_path,nn_path="agum_nn",n_epochs=1000):
+    frame_seqs=imgs.read_frame_seqs(in_path)
+    train,test=frame_seqs.split()
+    X,y,params=get_dataset(train)
+    print(params)
+    model=make_conv(params)
+    model.fit(X,y,epochs=n_epochs,batch_size=32)
+    if(nn_path):
+        model.save(nn_path)
+
+def filtr_seqs(in_path,out_path):
+    frame_seqs=imgs.read_frame_seqs(in_path)
+    frame_seqs.save(out_path)
+
+def get_dataset(frame_seqs):
+    X,y=[],[]
+    for name_i,seq_i in frame_seqs.items():
+        middle=int(len(seq_i)/2)
+        X+=[seq_i[0], seq_i[-1],seq_i[middle]]
+        y+=[0,0,1]
+    X,y=np.array(X),keras.utils.to_categorical(y)
+    params={'dims':X.shape[1:],'n_cats':y.shape[1]}
+    return X,y,params
+
+def make_conv(params):
+    model = Sequential()
+    model.add(Conv2D(16, kernel_size=(5, 5),
+                 activation='relu',
+                 input_shape=params['dims']))
+    model.add(MaxPooling2D(pool_size=(4, 4)))
+    model.add(Conv2D(16, kernel_size=(5, 5),
+                 activation='relu'))#,input_shape=(64,64,4)))
+    model.add(MaxPooling2D(pool_size=(4, 4)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu',name="hidden",kernel_regularizer=regularizers.l1(0.01),))
+    model.add(Dropout(0.5))
+    model.add(Dense(units=params['n_cats'], activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+              #optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True))
+              optimizer=keras.optimizers.Adadelta())
+    model.summary()
+    return model
+
+filtr_seqs("full","test2")
--- imgs.py
+++ imgs.py
@@ -1,12 +1,5 @@
-#import keras,keras.utils
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-#from keras.models import load_model
-from keras import regularizers
 import numpy as np
 import cv2
-import keras.utils
 import files
 
 class FrameSeqs(dict):
@@ -19,6 +12,12 @@ class FrameSeqs(dict):
     def split(self,selector=None):
         train,test=files.split(self,selector)
         return FrameSeqs(train),FrameSeqs(test)
+
+    def save(self,out_path):
+        files.make_dir(out_path)
+        for name_i,seq_i in self.items():
+            out_i="%s/%s" % (out_path,name_i)
+            save_frames(out_i,seq_i)
 
 def read_frame_seqs(in_path):
     frame_seqs=FrameSeqs()
@@ -33,27 +32,11 @@ def read_frame(in_path,n_split=3):
     frame_ij=cv2.imread(in_path,cv2.IMREAD_GRAYSCALE)
     return np.array(np.vsplit(frame_ij,n_split)).T
 
-def train_model(in_path,nn_path=None,n_epochs=5):
-    frame_seqs=read_frame_seqs(in_path)
-    train,test=frame_seqs.split()
-    X,y,params=get_dataset(train)
-    print(params)
-    model=make_conv(params)
-    model.fit(X,y,epochs=n_epochs,batch_size=32)
-    if(nn_path):
-        model.save(nn_path)
-
-def get_dataset(frame_seqs):
-    X,y=[],[]
-    for name_i,seq_i in frame_seqs.items():
-        middle=int(len(seq_i)/2)
-        X+=[seq_i[0], seq_i[-1],seq_i[middle]]
-        y+=[0,0,1]
-    X,y=np.array(X),keras.utils.to_categorical(y)
-#    params={'ts_len':X.shape[1],'n_feats':X.shape[2],
-#                'n_cats':y.shape[1],'n_channals':X.shape[-1]}
-    params={'dims':X.shape[1:],'n_cats':y.shape[1]}
-    return X,y,params
+def save_frames(in_path,frames):
+    files.make_dir(in_path)
+    for i,frame_i in enumerate(frames):
+        out_i="%s/%d.png" % (in_path,i)
+        cv2.imwrite(out_i, frame_i)
 #def extract_frame_feats(in_path,nn_path,out_path):
 #    model=load_model(nn_path)
 #    action_dirs=data.top_files(in_path)
@@ -71,26 +54,3 @@ def get_dataset(frame_seqs):
 #        out_i=out_path+'/'+action_i.split('/')[-1]
 #        pred_i=np.squeeze(pred_i, axis=1)
 #        np.savetxt(out_i, pred_i, delimiter=',' )
-
-def make_conv(params):
-    model = Sequential()
-    model.add(Conv2D(16, kernel_size=(5, 5),
-                 activation='relu',
-                 input_shape=params['dims']))
-    model.add(MaxPooling2D(pool_size=(4, 4)))
-    model.add(Conv2D(16, kernel_size=(5, 5),
-                 activation='relu'))#,input_shape=(64,64,4)))
-    model.add(MaxPooling2D(pool_size=(4, 4)))
-    model.add(Flatten())
-    model.add(Dense(100, activation='relu',name="hidden",kernel_regularizer=regularizers.l1(0.01),))
-    model.add(Dropout(0.5))
-    model.add(Dense(units=params['n_cats'], activation='softmax'))
-    model.compile(loss=keras.losses.categorical_crossentropy,
-              #optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True))
-              optimizer=keras.optimizers.Adadelta())
-    model.summary()
-    return model
-
-train_model("full")
-#frames=read_frame_seqs("full")
-#print( len(frames.split()[0]))
