a9117d44069864f747ea56747a51deb59c5a603c
tjacek <tjacek@agh.edu.pl>
2020-12-10 14:27:29 +0100

    improve DtwModel

--- one_shot.py
+++ one_shot.py
@@ -7,25 +7,30 @@ import numpy as np
 import feats,sim
 
 class DtwModel(object):
-    def __init__(self,first=100,last=64):
-        self.first=first
-        self.last=last
+    def __init__(self,n_feats):
+        self.n_feats=n_feats
 
     def __call__(self,model):
         activ='relu'
-        model.add(Dense(self.first, activation=activ))
-#        model.add(Dense(64, activation=activ))
+        for n_feats_i in self.n_feats[:-1]:
+            model.add(Dense(n_feats_i, activation=activ))
         model.add(Dropout(0.5))
-        model.add(Dense(self.last, activation=activ,name='hidden',
+        model.add(Dense(self.n_feats[-1], activation=activ,name='hidden',
             kernel_regularizer=regularizers.l1(0.01)))
         return model
+
+def get_basic_model():
+    return DtwModel([100,64])
+
+def get_large_model():
+    return DtwModel([128,96,64])
 
 def dtw_one_shot(in_path,out_path=None,n_epochs=5):
     dtw_feats=feats.read_feats(in_path)
     train,test=dtw_feats.split()
     X,y=to_sim_dataset(train)
     params={'input_shape':(dtw_feats.dim(),)}
-    siamese_net,extractor=sim.build_siamese(params,DtwModel())
+    siamese_net,extractor=sim.build_siamese(params,get_large_model())
     siamese_net.fit(X,y,epochs=n_epochs,batch_size=64)
     if(out_path):
         extractor.save(out_path)
@@ -37,7 +42,6 @@ def dtw_extract(in_path,nn_path,out_path):
     def helper(x_i):
         x_i=np.expand_dims(x_i,0)
         result_i= extractor.predict(x_i)
-#        raise Exception(result_i.shape)
         return result_i
     dtw_feats=dtw_feats.transform(helper)
     dtw_feats.save(out_path)
@@ -49,7 +53,6 @@ def to_sim_dataset(dtw_feats):
     for name_i,name_j in pairs:
         X.append((dtw_feats[name_i],dtw_feats[name_j]))
         y.append(int(name_i.split('_')[0]==name_j.split('_')[0]))
-    print(y)
     X=np.array(X)
     X=[X[:,0],X[:,1]]
 #    y=keras.utils.to_categorical(y)
