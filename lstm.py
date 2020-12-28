018672d585547dc42c768675778c8320c0a5a12e
tjacek <tjacek@agh.edu.pl>
2020-12-28 13:23:27 +0100

    agumented frame seqs

--- lstm.py
+++ lstm.py
@@ -10,22 +10,24 @@ import keras.utils,keras.optimizers
 import data.imgs,utils
 
 class SeqGenerator(keras.utils.Sequence):
-	def __init__(self,dataset,size=30,batch_size=32):
+	def __init__(self,dataset,size=30,batch_size=16,n_agum=2):
 		self.dataset=dataset
 		self.n_cats=self.dataset.n_cats()
 		self.agum=MinLength(size)
 		self.batch_size=batch_size
 		self.names=self.dataset.names()
+		self.n_agum=n_agum
 
 	def __len__(self):
 		return int(len(self.dataset)/self.batch_size)+1
 
 	def __getitem__(self, i):
-#		names=self.dataset.names()
 		names_i=self.names[i*self.batch_size:(i+1)*self.batch_size]
-		X=[ self.agum(self.dataset[name_j])  
-				for name_j in names_i]
-		y=[name_j.get_cat() for name_j in names_i ]
+		X,y=[],[]
+		for name_j in names_i:
+			for k in range(self.n_agum):
+				X.append(self.agum(self.dataset[name_j]))
+				y.append(name_j.get_cat())
 		y=utils.to_one_hot(y,self.n_cats)
 		return np.array(X),y
 
