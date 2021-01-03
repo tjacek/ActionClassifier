a071b0aad826c809f30d59919b802d02b2360e8f
tjacek <tjacek@student.agh.edu.pl>
2021-01-03 20:00:21 +0100

    retrain ae

--- ae.py
+++ ae.py
@@ -2,19 +2,20 @@ import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 print("physical_devices-------------", len(physical_devices))
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
-import numpy as np
+import numpy as np,os.path
 import keras.backend as K
 from keras.models import Model,Sequential
 from keras.layers import Input, Dense,Conv2D,Reshape,Conv2DTranspose
 from keras.layers import Flatten,MaxPooling2D,UpSampling2D
 from keras import regularizers
+from keras.models import load_model
 import data.imgs,utils,files
 
 def ae_exp(in_path,out_path,n_epochs=5):
 	files.make_dir(out_path)
-	paths=files.get_paths(out_path,["ae","feats"])
+	paths=files.get_paths(out_path,["ae","seqs"])
 	train_ae(in_path,paths['ae'],n_epochs)
-	extract(in_path,paths['ae'],paths['feats'])
+	extract(in_path,paths['ae'],paths['seqs'])
 
 def train_ae(in_path,out_path,n_epochs=5):
 	frames=data.imgs.read_frame_seqs(in_path,n_split=1)
@@ -22,7 +23,10 @@ def train_ae(in_path,out_path,n_epochs=5):
 	train.scale()
 	X=to_dataset(train)
 	params={'n_channels':X.shape[-1],"dim":(X.shape[1],X.shape[2]),'scale':(2,2)}
-	autoencoder,recon=make_autoencoder(params)
+	if(os.path.isfile(out_path)):
+		autoencoder=load_model(out_path)
+	else:
+		autoencoder,recon=make_autoencoder(params)
 	autoencoder.summary()
 	autoencoder.fit(X,X,epochs=n_epochs,batch_size=16)
 	autoencoder.save(out_path)
@@ -66,6 +70,4 @@ def make_autoencoder(params):
                       loss='mean_squared_error')#CustomLoss(autoencoder)
     return autoencoder,recon
 
-ae_exp("../3DHOI/box","../3DHOI/ae",n_epochs=5)
-#train_ae("../3DHOI/box","test")
-#extract("../3DHOI/box","test","ae_feats")
+ae_exp("../3DHOI_proj/proj","../3DHOI_proj/ae",n_epochs=50)
