e7cd682fbbc795305b54a99a238f84ea3bf8fae9
tjacek <tjacek@agh.edu.pl>
2021-02-05 15:32:12 +0100

    floor exp

--- clean/floor.py
+++ clean/floor.py
@@ -2,6 +2,15 @@ import sys
 sys.path.append("..")
 import numpy as np
 import clean,cnn
+import action_imgs,files
+
+def floor_exp(action_path,frame_path,dataset_path,
+				out_path,n_epochs=500):
+	files.make_dir(out_path)
+	paths=files.get_paths(out_path,["nn","frames","actions"])
+	train(action_path,dataset_path,paths["nn"],n_epochs)
+	apply_model(frame_path,paths["nn"],paths["frames"])
+	action_imgs.mean_action(paths["frames"],paths["actions"],None)
 
 def cut_floor(img_i,position):
 	position=int(position[0])
@@ -20,7 +29,7 @@ def show_dataset(frame_path,dataset_path,out_path):
 def train(frame_path,dataset_path,model_path,n_epochs=500):
 	X,y=clean.get_dataset(frame_path,dataset_path)
 	X=np.expand_dims(X,axis=-1)
-	img_shape=X.shape[1:]#(*X.shape[1:],1)
+	img_shape=X.shape[1:]
 	model=cnn.make_model(img_shape=img_shape,n_dense=1)
 	model.fit(X,y,epochs=n_epochs)
 	model.save(model_path)
@@ -29,9 +38,12 @@ def apply_model(frame_path,nn_path,out_path):
 	clean.apply_model(frame_path,nn_path,out_path,cut_floor)
 
 frame_path="../../clean/mean"
+
+action_path="../../clean/mean"
+frame_path="../../clean/frames"
 dataset_path="train_dataset"
-model_path="cnn"
+out_path="../../clean/floor"
+
 #make_dataset(frame_path,dataset_path)
 #show_dataset(frame_path,dataset_path,"test")
-#train(frame_path,dataset_path,model_path)
-apply_model("../../clean/frames",model_path,"../../clean/clean_frames")
+floor_exp(action_path,frame_path,dataset_path,out_path)
