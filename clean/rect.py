f25f1dd8a9521b7eba4a7992449558b190465c14
tjacek <tjacek@agh.edu.pl>
2021-02-09 09:50:50 +0100

    cut dataset

--- clean/rect.py
+++ clean/rect.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..")
 import numpy as np 
 import clean,files
-import action_imgs,cnn
+import action_imgs,cnn,data.imgs
 
 def rect_exp(action_path,frame_path,dataset_path,
 				out_path,n_epochs=1500):
@@ -15,7 +15,8 @@ def rect_exp(action_path,frame_path,dataset_path,
 def cut_rect(img_i,position):
 	if(np.product(position)==0):
 		return img_i
-	position=position.astype(int)
+	if(type(position)==np.ndarray):
+		position=position.astype(int)
 	x0,y0=position[0],position[1]
 	x1,y1=x0+position[2],y0+position[3]
 	img_i=img_i.copy()
@@ -42,11 +43,25 @@ def train(frame_path,dataset_path,model_path,n_epochs=500):
 def apply_model(frame_path,nn_path,out_path):
 	clean.apply_model(frame_path,nn_path,out_path,cut_rect)
 
-action_path="../../clean/floor/actions"
-dataset_path="../../clean/rect/dataset"
-frame_path="../../clean/floor/frames"
-action_path="../../clean/rect/actions"
-out_path="../../clean/rect/frames"
+def dataset_cut(dataset_path,frame_path,out_path):
+	dataset=clean.read(dataset_path)
+	frame_seqs=data.imgs.read_frame_seqs(frame_path,n_split=1)
+	new_seqs=data.imgs.FrameSeqs()
+	for name_i in dataset.keys():
+		seq_i=frame_seqs[name_i]
+		position_i=dataset[name_i]
+		new_seqs[name_i]=[cut_rect(frame_j,position_i) 
+						for frame_j in seq_i]
+	new_seqs.save(out_path)
+
+in_dir="rect2"
+out_dir="rect3"
+action_path="../../clean/%s/actions" % in_dir
+dataset_path="../../clean/%s/dataset" % out_dir
+frame_path="../../clean/%s/frames" % in_dir
+action_path="../../clean/%s/actions" % in_dir
+out_path="../../clean/%s/frames" % out_dir
 #make_dataset(action_path,dataset_path)
 #show_dataset(action_path,dataset_path,action_path)
-rect_exp(action_path,frame_path,dataset_path,out_path)
+#rect_exp(action_path,frame_path,dataset_path,out_path)
+dataset_cut(dataset_path,frame_path,"frames")
