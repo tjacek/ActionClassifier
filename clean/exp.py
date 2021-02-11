4512091b10d4ea0ac9b0f057c0e9d963253e22c8
tjacek <tjacek@agh.edu.pl>
2021-02-11 16:13:20 +0100

    gen_frames

--- clean/exp.py
+++ clean/exp.py
@@ -1,12 +1,25 @@
 import sys
 sys.path.append("..")
 import files,tools,action_imgs
+import rect
 
-def bound_exp(in_path,out_path):
-	paths=files.get_paths(out_path,["actions","bounds"])
-	action_imgs.mean_action(in_path,paths["actions"],dims=None)
+def gen_frames(old_frames,dataset_path,dir_path):
+	files.make_dir(dir_path)
+	new_frames="%s/frames" % dir_path
+	rect.dataset_cut(dataset_path,old_frames,new_frames)
+	show_frames(new_frames,dir_path)
+
+def show_frames(frame_path,dir_path):
+	paths=files.get_paths(dir_path,["actions","bounds"])
+	action_imgs.mean_action(frame_path,paths["actions"],dims=None)
 	tools.cut_actions(paths["actions"],paths["bounds"],scale=None,binary=True)
 
-in_path="../../clean/frames"
-out_path="../../clean"
-bound_exp(in_path,out_path)
+frame_path="../../clean/frames"
+dir_path="../../clean"
+action_path="../../clean/actions"
+dataset_path="../../clean/dataset"
+exp_path=dir_path+"/exp1"
+
+#bound_exp(frame_path,dir_path)
+rect.make_dataset(action_path,dataset_path)
+#gen_frames(frame_path,dataset_path,exp_path)
