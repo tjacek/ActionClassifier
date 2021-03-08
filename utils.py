09e8d0ef5c494297af48d5b64b5d71c77cb3856b
tjacek <tjacek@agh.edu.pl>
2021-03-08 10:30:35 +0100

    cat sizes

--- utils.py
+++ utils.py
@@ -87,6 +87,20 @@ def seq_stats(in_path):
     stats=(len(seqs_len),frames.n_persons())
     print("n_seqs:%s\nn_persons:%s\n" % stats)
 
+def mean_action_size(in_path):
+    frames=data.imgs.read_frame_seqs(in_path,n_split=1)
+    cats={}
+    for name_i,seq_i in frames.items():
+        cat_i=name_i.get_cat()
+        if( name_i.get_person()%2==1):
+            if(not cat_i in cats ):
+                cats[cat_i]=0
+            cats[cat_i]+=len(seq_i)
+    print(cats)
+    values=list(cats.values())
+    print(np.median(values))
+    print(sum(values))
+
 if __name__=="__main__":
 #    check_model("../action/ens/nn/0")
-    seq_stats('../3DHOI')
+    mean_action_size('../../common/frames/full')
