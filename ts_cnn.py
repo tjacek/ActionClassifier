8fa7e310d38c3f3aac15dcdd74921754b864783c
tjacek <tjacek@agh.edu.pl>
2021-03-13 22:32:07 +0100

    ts cnn class

--- ts_cnn.py
+++ ts_cnn.py
@@ -12,6 +12,34 @@ from keras import regularizers
 from keras.models import load_model
 import files,spline,data.seqs,utils,ens,sim
 
+class TS_CNN(object):
+    def __init__(self,nn_type):
+        self.nn_type=nn_type
+
+    def __call__(self,params):
+        activ='relu'
+        input_img=Input(shape=(params['ts_len'], params['n_feats']))
+        n_kerns,kern_size,pool_size=[128,128],[8,8],[4,2]
+        if(self.nn_type=="narrow"):
+            x=Conv2D(32,kernel_size=(8, 1),activation='relu')(input_img)
+            x=MaxPooling2D(pool_size=(4, 1))(x)
+            x=Conv2D(32, kernel_size=(8, 1),activation='relu')(x)
+            x=MaxPooling2D(pool_size=(4, 1))(x)
+        else:
+            x=Conv1D(n_kerns[0], kernel_size=kern_size[0],activation=activ,name='conv1')(input_img)
+            x=MaxPooling1D(pool_size=pool_size[0],name='pool1')(x)
+            x=Conv1D(n_kerns[1], kernel_size=kern_size[1],activation=activ,name='conv2')(x)
+            x=MaxPooling1D(pool_size=pool_size[1],name='pool2')(x)
+        x=Flatten()(x)
+        x=Dense(100, activation='relu',name="hidden",kernel_regularizer=regularizers.l1(0.01),)(x)
+        x=Dropout(0.5)(x)
+        x=Dense(units=params['n_cats'],activation='softmax')(x)
+        model = Model(input_img, x)
+        model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True))
+        model.summary()
+        return model
+
 def simple_exp(in_path,n_epochs=1000):
     print(paths)
     train=get_train(nn_type="wide")
@@ -30,52 +58,13 @@ def ensemble_exp(in_path,out_name,n_epochs=1000,size=64):
 
 def get_train(nn_type="wide"):
     read=data.seqs.read_seqs
-    if(nn_type=="narrow"):
-        return utils.TrainNN(read,make_narrow1D,to_dataset)
-    return utils.TrainNN(read,make_wide1D,to_dataset)
+    return utils.TrainNN(read,TS_CNN(nn_type),to_dataset)
 
 def to_dataset(seqs):
     X,y=seqs.to_dataset()
     n_cats=max(y)+1
     params={'ts_len':X.shape[1],'n_feats':X.shape[2],'n_cats':n_cats}
     return X,y,params
-
-def make_wide1D(params):
-    x,input_img=basic_model(params)
-    x=Dropout(0.5)(x)
-    x=Dense(units=params['n_cats'],activation='softmax')(x)
-    model = Model(input_img, x)
-    model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True))
-    model.summary()
-    return model
-
-def basic_model(params):
-    activ='relu'
-    input_img=Input(shape=(params['ts_len'], params['n_feats']))
-    n_kerns,kern_size,pool_size=[128,128],[8,8],[4,2]
-    x=Conv1D(n_kerns[0], kernel_size=kern_size[0],activation=activ,name='conv1')(input_img)
-    x=MaxPooling1D(pool_size=pool_size[0],name='pool1')(x)
-    x=Conv1D(n_kerns[1], kernel_size=kern_size[1],activation=activ,name='conv2')(x)
-    x=MaxPooling1D(pool_size=pool_size[1],name='pool2')(x)
-    x=Flatten()(x)
-    x=Dense(100, activation='relu',name="hidden",kernel_regularizer=regularizers.l1(0.01),)(x)
-    return x,input_img
-
-def make_narrow1D(params):
-    input_img=Input(shape=(params['ts_len'], params['n_feats'],1))
-    x=Conv2D(32,kernel_size=(8, 1),activation='relu')(input_img)
-    x=MaxPooling2D(pool_size=(4, 1))(x)
-    x=Conv2D(32, kernel_size=(8, 1),activation='relu')(x)
-    x=MaxPooling2D(pool_size=(4, 1))(x)
-    x=Flatten()(x)
-    x=Dense(100, activation='relu',name="hidden",kernel_regularizer=regularizers.l1(0.01),)(x)
-    x=Dense(units=params['n_cats'],activation='softmax')(x)
-    model = Model(input_img, x)
-    model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True))
-    model.summary()
-    return model
     
 def narrow_read(in_path):
     seq_dict=seqs.read_seqs(in_path)
@@ -84,5 +73,5 @@ def narrow_read(in_path):
     return seqs.Seqs(seq_dict)
 
 if __name__ == "__main__":
-    ensemble_exp("../dtw_paper/MSR/binary","1D_CNN_tests",n_epochs=10)
+    ensemble_exp("../dtw_paper/MSR/binary","1D_CNN_tests",n_epochs=1000)
 #    binary_exp("../dtw_paper/MHAD/binary/","../dtw_paper/MHAD/binary/1D_CNN_128")
