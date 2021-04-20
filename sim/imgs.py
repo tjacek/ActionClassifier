89b842d1a2b7c1b956c2e82e06a38797e417cc7d
tjacek <tjacek@student.agh.edu.pl>
2021-04-20 23:52:04 +0200

    fix SimConv class

--- sim/imgs.py
+++ sim/imgs.py
@@ -4,9 +4,10 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras import regularizers
 
 class SimConv(object):
-    def __init__(self,name="hidden",activ='relu'):
+    def __init__(self,name="hidden",activ='relu',l1=0.01):
         self.activ=activ
         self.name=name
+        self.l1=l1
 
     def __call__(self,model,params):
         model.add(Conv2D(64, kernel_size=(5, 5),
@@ -16,27 +17,7 @@ class SimConv(object):
                  activation=self.activ))
         model.add(MaxPooling2D(pool_size=(4, 4)))
         model.add(Flatten())
-#        name=params["name"] if(params['name']) else "hidden"
+        reg= None if(self.l1 is None) else regularizers.l1(0.01)
         model.add(Dense(128, activation=self.activ,name=self.name,
-            kernel_regularizer=regularizers.l1(0.01),))
+            kernel_regularizer=reg,))
         return model
-
-
-#def make_conv(model,params):
-#    params['name']="large_dense"
-#    model=base_conv(model,params)
-#    model.add(Dense(128, activation='relu',name="hidden"))
-#    return model
-
-#def base_conv(model,params):
-#    model.add(Conv2D(64, kernel_size=(5, 5),
-#                 activation='relu'))
-#    model.add(MaxPooling2D(pool_size=(4, 4)))
-#    model.add(Conv2D(32, kernel_size=(5, 5),
-#                 activation='relu'))
-#    model.add(MaxPooling2D(pool_size=(4, 4)))
-#    model.add(Flatten())
-#    name=params["name"] if(params['name']) else "hidden"
-#    model.add(Dense(128, activation='relu',name=name,
-#    	kernel_regularizer=regularizers.l1(0.01),))
-#    return model
