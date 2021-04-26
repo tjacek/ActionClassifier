e2ed26565402f14ad348ae5a6fd0ae529ce31a7b
tjacek <tjacek@student.agh.edu.pl>
2021-04-26 15:14:20 +0200

    refactore lstm

--- lstm.py
+++ lstm.py
@@ -27,9 +27,11 @@ class MinLength(object):
 		return [frames[i] for i in indexes]
 
 class FRAME_LSTM(object):
-	def __init__(self,dropout=0.5,activ='relu'):
+	def __init__(self,dropout=0.5,activ='elu',batch=False,l1=0.01):
 		self.dropout=dropout
 		self.activ=activ
+		self.batch=batch
+		self.l1=l1
 
 	def __call__(self,params):
 		input_shape= (params['seq_len'],*params['dims']) 
@@ -43,12 +45,17 @@ class FRAME_LSTM(object):
 		model.add(TimeDistributed(Dense(256)))
 		if( not (self.dropout is None)):
 			model.add(TimeDistributed(Dropout(self.dropout)))	
+		reg=None if(self.l1  is None) else regularizers.l1(self.l1)
 		model.add(TimeDistributed(Dense(128, name="first_dense",
-				kernel_regularizer=regularizers.l1(0.01))))
+				activation=self.activ,kernel_regularizer=reg)))
 
-		model.add(LSTM(64, return_sequences=True, name="lstm_layer"));
-		model.add(GlobalAveragePooling1D(name="prebatch"))
-		model.add(BatchNormalization(name="global_avg"))
+		model.add(LSTM(64, return_sequences=True, name="lstm_layer",activation=self.activ));
+		
+		if(self.batch):
+			model.add(GlobalAveragePooling1D(name="prebatch",activation=self.activ))
+			model.add(BatchNormalization(name="global_avg"))
+		else:
+			model.add(GlobalAveragePooling1D(name="global_avg"))
 		model.add(Dense(params['n_cats'],activation='softmax'))
 
 		model.compile(loss='categorical_crossentropy',
@@ -94,41 +101,63 @@ def extract(in_path,nn_path,out_path,seq_len=30):
 
 def binary_lstm(in_path,out_path,n_epochs=5,seq_len=20):
 	n_cats=20
-	binary_gen=static_binary(in_path,n_epochs,seq_len)
+	binary_gen=get_binary(in_path,n_epochs,seq_len)
 	funcs=[[extract,["in_path","nn","feats"]]]
 	dir_names=["feats"]
 	arg_dict={'in_path':in_path}		
 	binary_ens=ens.BinaryEns(binary_gen,funcs,dir_names)
 	binary_ens(out_path,n_cats,arg_dict)
 
-def static_binary(in_path,n_epochs=5,seq_len=20,n_channels=3):
+def get_binary(in_path,n_epochs=5,seq_len=20,n_channels=3,static=True):
 	dataset=data.imgs.read_frame_seqs(in_path,n_split=n_channels)
 	train,test=dataset.split()
-	train.transform(MinLength(seq_len),single=False)
+	if(static):
+		train.transform(MinLength(seq_len),single=False)
+	else:
+		seq_gen=gen.SeqGenerator(train,MinLength(seq_len),batch_size=4,n_agum=1,binary=0)
 	train.scale()
-	params={'n_cats':2,"seq_len":seq_len,"dims":train.dims()}#,"drop":False}
+	params={'n_cats':2,"seq_len":seq_len,"dims":train.dims(),"drop":False}
 	X,y=train.to_dataset()
 	make_lstm=FRAME_LSTM()
 	def binary_train(nn_path,i):
 		y_i=ens.binarize(y,i)	
+		model=make_lstm(params)
+		if(static):
+			model.fit(X,y_i,epochs=n_epochs,batch_size=4)
+		else:
+			model.fit_generator(seq_gen,epochs=n_epochs)
+		model.save(nn_path)
+	return binary_train		
+
+
+#def static_binary(in_path,n_epochs=5,seq_len=20,n_channels=3):
+#	dataset=data.imgs.read_frame_seqs(in_path,n_split=n_channels)
+#	train,test=dataset.split()
+#	train.transform(MinLength(seq_len),single=False)
+#	train.scale()
+#	params={'n_cats':2,"seq_len":seq_len,"dims":train.dims(),"drop":False}
+#	X,y=train.to_dataset()
+#	make_lstm=FRAME_LSTM()
+#	def binary_train(nn_path,i):
+#		y_i=ens.binarize(y,i)	
 #		model=make_lstm(params)
 #		model.fit(X,y_i,epochs=n_epochs,batch_size=4)
 #		model.save(nn_path)
-	return binary_train		
+#	return binary_train		
 
-def dynamic_binary(in_path,n_epochs=5,seq_len=20):
-	frames=data.imgs.read_frame_seqs(in_path,n_split=3)
-	train,test=frames.split()
-	train.scale()
-	params={'n_cats':2,"seq_len":seq_len,"dims":train.dims()}
-	seq_gen=gen.SeqGenerator(train,MinLength(params['seq_len']),batch_size=4,n_agum=1,binary=0)
-	make_lstm=FRAME_LSTM()
-	def binary_train(nn_path,i):
-		seq_gen.binary=i
-		model=make_lstm(params)
-		model.fit_generator(seq_gen,epochs=n_epochs)
-		model.save(nn_path)
-	return binary_train
+#def dynamic_binary(in_path,n_epochs=5,seq_len=20):
+#	frames=data.imgs.read_frame_seqs(in_path,n_split=3)
+#	train,test=frames.split()
+#	train.scale()
+#	params={'n_cats':2,"seq_len":seq_len,"dims":train.dims()}
+#	seq_gen=gen.SeqGenerator(train,MinLength(params['seq_len']),batch_size=4,n_agum=1,binary=0)
+#	make_lstm=FRAME_LSTM()
+#	def binary_train(nn_path,i):
+#		seq_gen.binary=i
+#		model=make_lstm(params)
+#		model.fit_generator(seq_gen,epochs=n_epochs)
+#		model.save(nn_path)
+#	return binary_train
 
 def lstm_exp(in_path,out_path,n_epochs=200,seq_len=20,gen=False):
 	files.make_dir(out_path)
@@ -140,7 +169,7 @@ def lstm_exp(in_path,out_path,n_epochs=200,seq_len=20,gen=False):
 	extract(in_path,paths['nn'],paths['feats'],seq_len)
 
 if __name__ == "__main__":
-	binary_lstm('../MSR/full','../MSR/lstm3',n_epochs=20,seq_len=30)
+	binary_lstm('../MSR/full','../MSR/lstm4',n_epochs=20,seq_len=30)
 #	train_lstm('../3DHOI/frames','../3DHOI/nn',n_epochs=200,seq_len=20)
 #	extract('../3DHOI/frames','../3DHOI/nn','../3DHOI/feats',seq_len=20)
 #	binary_lstm("../MSR/frames","../MSR/ens4",n_epochs=250,seq_len=20)
