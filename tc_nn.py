46de0c0a67d8a1b296159d33b90233e41e398bb3
tjacek <tjacek@agh.edu.pl>
2021-06-03 22:57:35 +0200

    simple tcn exp

--- tc_nn.py
+++ tc_nn.py
@@ -1,12 +1,26 @@
 from tcn import TCN, tcn_full_summary
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
-
+import data.seqs,utils
 
 def make_tcn(params):
-    tcn_layer = TCN(input_shape=(params["time_steps"], params["input_dim"]))
+    tcn_layer = TCN(input_shape=(params["ts_len"], params["n_feats"]))
     m = Sequential([tcn_layer,Dense(1)])
     m.compile(optimizer='adam', loss='mse')
     tcn_full_summary(m, expand_residual_blocks=False)
 
-make_tcn({"time_steps":50,"input_dim":10})
+def simple_exp(in_path,out_path,n_epochs=1000):
+    ts_cnn=make_tcn
+    read=data.seqs.read_seqs
+    train=utils.TrainNN(read,ts_cnn,to_dataset)
+    extract=utils.Extract(read)
+    utils.single_exp_template(in_path,out_path,train,extract)
+
+def to_dataset(seqs):
+    X,y=seqs.to_dataset()
+    n_cats=max(y)+1
+    params={'ts_len':X.shape[1],'n_feats':X.shape[2],'n_cats':n_cats}
+    return X,y,params
+
+in_path="../3DHOI/spline"
+simple_exp(in_path,"test")
