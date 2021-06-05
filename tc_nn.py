110616cbaa6948abf8d502d82efbce3b8654b9d3
tjacek <tjacek@agh.edu.pl>
2021-06-05 23:58:25 +0200

    extrct tc_nn

--- tc_nn.py
+++ tc_nn.py
@@ -11,18 +11,44 @@ def make_tcn(params):
     tcn_full_summary(m, expand_residual_blocks=False)
     return m
 
-def simple_exp(in_path,out_path,n_epochs=1000):
-    ts_cnn=make_tcn
-    read=data.seqs.read_seqs
-    train=utils.TrainNN(read,ts_cnn,to_dataset)
-    extract=utils.Extract(read)
-    utils.single_exp_template(in_path,out_path,train,extract)
+def simple_exp(in_path,out_path,n_epochs=100):
+    train,extract=train_tcn,extract_tcn
+    utils.single_exp_template(in_path,out_path,train,extract,
+        n_epochs=n_epochs)
+
+def train_tcn(in_path,nn_path,n_epochs=5):
+    dataset=data.seqs.read_seqs(in_path)
+    train,test=dataset.split()
+    X,y,params=to_dataset(train)
+    if("n_cats" in params ):
+        y=utils.to_one_hot(y,params["n_cats"])
+    model=make_tcn(params)
+    model.fit(X,y,epochs=n_epochs,batch_size=32)
+    model.save_weights(nn_path)
+
+def extract_tcn(in_path,nn_path,out_path):
+    dataset=data.seqs.read_seqs(in_path)
+    X,y,params=to_dataset(dataset)
+    model=make_tcn(params)
+    model.load_weights(nn_path)    
+    raise Exception("OK")
+#        model=load_model(nn_path)
+#        extractor=Model(inputs=model.input,
+#                outputs=model.get_layer(self.name).output)
+#        extractor.summary()
+#        names=dataset.names()
+#        feat_seqs=data.seqs.Seqs()
+#        for i,name_i in enumerate(dataset.names()):
+#            x_i=np.array(dataset[name_i])
+#            feat_seqs[name_i]= extractor.predict(x_i)
+#        feat_seqs.save(out_path)
 
 def to_dataset(seqs):
     X,y=seqs.to_dataset()
     n_cats=max(y)+1
     params={'ts_len':X.shape[1],'n_feats':X.shape[2],'n_cats':n_cats}
     return X,y,params
+
 
 in_path="../3DHOI/spline"
 simple_exp(in_path,"test")
