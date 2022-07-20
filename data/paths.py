a555b4fd832b0c39d9be2b7a4f9f4b8fcf4dce52
tjacek <tjacek@agh.edu.pl>
2022-07-21 01:07:17 +0200

    paths reader

--- data/paths.py
+++ data/paths.py
@@ -1,13 +1,22 @@
-from . import DataDict
+import os
+import files
+from . import DataDict,imgs
 
 class PathsDict(DataDict):
-    def __init__(self, args=[]):
+    def __init__(self, args=[],reader=None):
         super(DataDict, self).__init__(args)
+        if(reader is None):
+            reader= imgs.ReadFrames()
+        self.reader=reader
 
     def read(self,name_i):
-        return imgs.read_frames(self[name_i])
-
+        frames=[self.reader(path_i) 
+            for path_i in self[name_i]]
+        return np.array(frames)
 
 def read_paths(in_path):
     path_dict=PathsDict()
-    
+    for name_i in os.listdir(in_path):
+        path_i=f"{in_path}/{name_i}"
+        path_dict[name_i]=files.top_files(path_i)  
+    return path_dict
--- eff_lstm.py
+++ eff_lstm.py
@@ -1,2 +1,8 @@
 import data.paths
 
+def train(in_path):
+    path_dict=data.paths.read_paths(in_path)
+    print(path_dict)
+
+in_path="../CZU-MHAD/CZU-MHAD/final"
+train(in_path)
